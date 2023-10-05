09cb3a663346125eadf77be968bf5ec3e233626e
Matthew Cavallo <macavall@microsoft.com>
2023-10-05 16:17:28 -0500

    first

--- api/message.cs
+++ api/message.cs
@@ -1,17 +1,19 @@
-using System;
-using System.IO;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.AspNetCore.Http;
+using System.Net;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
 
 namespace Company.Function
 {
     public static class message
     {
+        private readonly ILogger _logger;
+
+        public message(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<message>();
+        }
+
         [FunctionName("message")]
         public static async Task<string> Run(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
