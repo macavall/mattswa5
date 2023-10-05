8c02e61673cff44fca6bd651f8f783101a52298a
Matthew Cavallo <macavall@microsoft.com>
2023-10-05 16:11:46 -0500

    first

---
+++ api/message.cs
@@ -0,0 +1,27 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace Company.Function
+{
+    public static class message
+    {
+        [FunctionName("message")]
+        public static async Task<string> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            System.Threading.Thread.Sleep(20000);
+
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+
+            return "Complete";
+        }
+    }
+}
